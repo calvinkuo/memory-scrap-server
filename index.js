af9c5562d044fbd22b4aaaff1828a8f9afeb0f18
calvinkuo <2019ckuo@tjhsst.edu>
2018-10-04 11:11:17 -0400

    Implement Memory Scrap page

--- index.js
+++ index.js
@@ -9,27 +9,12 @@ const pool = new Pool({
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
   .get('/create', async (req, res) => {
     try {
       const client = await pool.connect()
       await client.query('create table test_table (id integer, name text)');
       await client.query('insert into test_table values (1, \'hello database\')');
-      res.render('pages/index');
+      res.send("Success");
       client.release();
     } catch (err) {
       console.error(err);
